1226da6e6179eb8ff61e3a44e4eaf882aa0dbdb6
yashvant <yaswanth.k23@gmail.com>
2024-03-04 17:35:47 +0530

    ping api and basic set up changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module, NestModule, MiddlewareConsumer } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { APP_INTERCEPTOR } from "@nestjs/core";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { LoggingInterceptor } from "./middleware/logging.interceptor";
+import { JsonHeaderInterceptor } from "./middleware/jsonHeader.interceptor";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: JsonHeaderInterceptor,
+    },
+  ],
 })
 export class AppModule {}
---
+++ src/middleware/logging.interceptor.ts
@@ -0,0 +1,37 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from "@nestjs/common";
+import { Observable } from "rxjs";
+import { tap } from "rxjs/operators";
+import { Logger } from "../common/logger";
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  private readonly logger = Logger(LoggingInterceptor.name);
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request = context.switchToHttp().getRequest();
+    const method = request.method;
+    const url = request.url;
+    const now = Date.now();
+
+    this.logger.info(
+      `Test ${request.hostname} ${request.port || ""} ${request.originalUrl}`
+    );
+    return next.handle().pipe(
+      tap(() => {
+        const response = context.switchToHttp().getResponse();
+        const statusCode = response.statusCode;
+
+        this.logger.info(
+          `${method} ${url} ${statusCode} - ${
+            Date.now() - now
+          }ms, Date: ${new Date().toISOString()}`
+        );
+      })
+    );
+  }
+}
